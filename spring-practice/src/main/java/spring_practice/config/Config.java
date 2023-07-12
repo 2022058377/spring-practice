2e06c4f5bf466befa66c5f2952214b96305073f2
JAEWON <bjo3963@naver.com>
2023-07-12 12:15:25 +0900

    Web #1 - spring webmvc tomcat

--- spring-practice/src/main/java/spring_practice/config/Config.java
+++ spring-practice/src/main/java/spring_practice/config/Config.java
@@ -2,7 +2,9 @@ package spring_practice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("spring_practice")
 @Configuration
 public class Config {
